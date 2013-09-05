0e27b45935b4971cb8c6aed89316ec74e4675573
bstuder <trosk@laposte.net>
2013-09-05 18:13:53 +0200

    build fix

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "org.postgresql" % "postgresql" % "9.2-1003-jdbc4"
-      ,"com.roundeights" % "hasher" % "0.3" //from "http://cloud.github.com/downloads/Nycto/Hasher/hasher_2.9.1-0.3.jar"
+      ,"com.roundeights" % "hasher" % "0.3" from "http://cloud.github.com/downloads/Nycto/Hasher/hasher_2.9.1-0.3.jar"
       ,"org.apache.poi" % "poi" % "3.9"
       ,"org.apache.poi" % "poi-ooxml" % "3.9"
     )
